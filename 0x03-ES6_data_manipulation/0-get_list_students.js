8d3e315e1e29c680498184b53eee163c973e5a2c
Perseverance Mutengera <123206539+Persemutengera@users.noreply.github.com>
2024-03-12 20:34:41 +0200

    Update 0-get_list_students.js

--- 0x03-ES6_data_manipulation/0-get_list_students.js
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -1,6 +1,6 @@
 /**
  * Retrieves a list of students.
- * @author Bezaleel Olakunori <https://github.com/B3zaleel>
+ * @author Perseverance Mutengera <https://github.com/Persemutengera>
  * @returns {{id: Number, firstName: String, location: String}[]}
  */
 export default function getListStudents() {
