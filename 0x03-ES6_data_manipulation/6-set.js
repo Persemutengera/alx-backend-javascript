32b163425f235292788a214895a854f330807292
Perseverance Mutengera <123206539+Persemutengera@users.noreply.github.com>
2024-03-12 20:23:46 +0200

    Update 6-set.js

--- 0x03-ES6_data_manipulation/6-set.js
+++ 0x03-ES6_data_manipulation/6-set.js
@@ -1,7 +1,7 @@
 /**
  * Creates a set from an array.
  * @param {Array} array - The source array.
- * @author Bezaleel Olakunori <https://github.com/B3zaleel>
+ * @author Perseverance Mutengera <https://github.com/Persemutengera>
  * @returns {Set}
  */
 export default function setFromArray(array) {
