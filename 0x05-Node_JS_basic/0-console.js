3b4b530f7f96d6e764bffb3008b0d2be9919a08f
Perseverance Mutengera <123206539+Persemutengera@users.noreply.github.com>
2024-04-30 11:37:01 +0200

    Update 0-console.js

--- 0x05-Node_JS_basic/0-console.js
+++ 0x05-Node_JS_basic/0-console.js
@@ -1,7 +1,7 @@
 /**
  * Displays a message on the STDOUT.
  * @param {String} msg The message to display.
- * @author Bezaleel Olakunori <https://github.com/B3zaleel>
+ * @author Perseverance Mutengera <https://github.com/Persemutengera>
  */
 const displayMessage = (msg) => {
   console.log(msg);
